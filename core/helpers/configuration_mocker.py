5b8ed8d0217ef550f90830be4c7a56d7b3d507ba
Ethan Knox <emk@integrichain.com>
2019-01-20 17:33:56 -0500

    helper supports full model definition

--- core/helpers/configuration_mocker.py
+++ core/helpers/configuration_mocker.py
@@ -10,7 +10,7 @@ class ConfigurationMocker:
 
     def __init__(self):
         # TODO: migrate to local postgres instance
-        engine = create_engine('sqlite:///:in-memory')
+        engine = create_engine('sqlite://')
         #engine = config.GenEngine(env='dev', local=True).get_engine()
 
         # this instansiates the in-memory sqlite instance
@@ -23,9 +23,109 @@ class ConfigurationMocker:
         return self.session
 
     def generate_mocks(self):
+        self._mock_pharmaceutical_companies()
+        self._mock_brands()
+        self._mock_segments()
+        self._mock_pipeline_types()
+        self._mock_pipelines()
+        self._mock_pipeline_state_types()
+        self._mock_pipeline_states()
         self._mock_transformation_templates()
         self._mock_transformations()
         self._mock_extract_configurations()
+
+    def _mock_brands(self):
+        logging.debug('Generating brand mocks...')
+        b = config.Brand
+        self.session.add_all([
+        b(id=1, name = "Teamocil", display_name = "Teamocil", pharmaceutical_company_id = 1),
+        b(id=2, name = "Cornballer", display_name = "Corn Baller", pharmaceutical_company_id = 2)])
+        self.session.commit()
+        logging.debug('Done generating brand mocks.')
+
+    def _mock_extract_configurations(self):
+        logging.debug('Generating extract_configuation mocks...')
+        ex = config.ExtractConfiguration
+        self.session.add_all([
+            ex(id=1, transformation_id=2, filesystem_path='',
+               prefix='', secret_name='bluth'),
+            ex(id=2, transformation_id=2, filesystem_path='banana_stand_data',
+               prefix='gob', secret_name='bluth'),
+            ex(id=3, transformation_id=3, filesystem_path='sudden_valley_holdings',
+               prefix='', secret_name='bluth'),
+            ex(id=4, transformation_id=1, filesystem_path='',
+               prefix='', secret_name='sitwell'),
+            ex(id=5, transformation_id=1, filesystem_path='',
+               prefix='001545', secret_name='sitwell'),
+            ex(id=6, transformation_id=1, filesystem_path='200-1',
+               prefix='', secret_name='sitwell')
+        ])
+        self.session.commit()
+        logging.debug('Done generating extract_configuration mocks.')
+
+    def _mock_pharmaceutical_companies(self):
+        logging.debug('Generating pharmaceutical company mocks...')
+        p = config.PharmaceuticalCompany
+        self.session.add_all([
+        p(id=1, display_name = "Natural Life Food Company", name = 'Nfoods'),
+        p(id=2, display_name = "Sitwell Home Construction", name = "Sitwell")])
+        self.session.commit()
+        logging.debug('Done generating pharmaceutical company mocks.')
+
+    def _mock_pipelines(self):
+        logging.debug('Generating pipeline mocks...')
+        p = config.Pipeline
+        self.session.add_all([
+        p(id=1, name = "bluth_banana_regression", brand_id=2, pipeline_type_id =1, run_frequency= 'daily'),
+        p(id=2, name = "bluth_profitability", brand_id=2, pipeline_type_id =2, run_frequency= 'hourly'),
+        p(id=3, name = "temocil_profitablility", brand_id=1, pipeline_type_id=1, run_frequency='daily')])
+        self.session.commit()
+        logging.debug('Done generating pipeline mocks.')
+
+    def _mock_pipeline_states(self):
+        logging.debug('Generating pipeline state mocks...')
+        p = config.PipelineState
+        self.session.add_all([
+        p(id=1, pipeline_state_type_id = 1, pipeline_id =1, graph_order=0),
+        p(id=2, pipeline_state_type_id = 2, pipeline_id =1, graph_order=1),
+        p(id=3, pipeline_state_type_id = 3, pipeline_id =1, graph_order=2),
+        p(id=4, pipeline_state_type_id = 1, pipeline_id =2, graph_order=0)])
+        self.session.commit()
+        logging.debug('Done generating pipeline state mocks.')
+
+    def _mock_pipeline_state_types(self):
+        logging.debug('Generating pipeline state type mocks...')
+        p = config.PipelineStateType
+        self.session.add_all([
+        p(id=1, name = "Raw"),
+        p(id=2, name = "Ingest"),
+        p(id=3, name = "Master"),
+        p(id=4, name = "Enhance"),
+        p(id=5, name = "Enrich"),
+        p(id=6, name = "Metrics"),
+        p(id=7, name = "Dimensional")])
+        self.session.commit()
+        logging.debug('Done generating pipeline state type mocks.')
+
+
+    def _mock_pipeline_types(self):
+        logging.debug('Generating pipeline type mocks...')
+        p = config.PipelineType
+        self.session.add_all([
+        p(id=1, name = "regression", segment_id = 1),
+        p(id=2, name = "profitability", segment_id = 2)])
+        self.session.commit()
+        logging.debug('Done generating pipeline type mocks.')
+
+    def _mock_segments(self):
+        logging.debug('Generating segment mocks...')
+        s = config.Segment
+        self.session.add_all([
+        s(id=1, name = "Patient"),
+        s(id=2, name = "Payer"),
+        s(id=3, name = "Distribution")])
+        self.session.commit()
+        logging.debug('Done generating segment mocks.')
 
     def _mock_transformations(self):
         logging.debug('Generating transformation mocks...')
@@ -41,25 +141,7 @@ class ConfigurationMocker:
         self.session.commit()
         logging.debug('Done generating transformation mocks.')
 
-    def _mock_extract_configurations(self):
-        logging.debug('Generating extract_configuation mocks...')
-        ex = config.ExtractConfiguration
-        self.session.add_all([
-            ex(id=1, transformation_id=2, filesystem_path='',
-               prefix='', secret_name='bluth'),
-            ex(id=2, transformation_id=2, filesystem_path='banana_stand',
-               prefix='gob', secret_name='bluth'),
-            ex(id=3, transformation_id=3, filesystem_path='sudden_valley',
-               prefix='', secret_name='bluth'),
-            ex(id=4, transformation_id=1, filesystem_path='',
-               prefix='', secret_name='sitwell'),
-            ex(id=5, transformation_id=1, filesystem_path='',
-               prefix='001545', secret_name='sitwell'),
-            ex(id=6, transformation_id=1, filesystem_path='200-1',
-               prefix='', secret_name='sitwell')
-        ])
-        self.session.commit()
-        logging.debug('Done generating extract_configuration mocks.')
+
 
     def _mock_transformation_templates(self):
         logging.debug('Generating transformation_template mocks...')
--- core/models/configuration.py
+++ core/models/configuration.py
@@ -18,9 +18,11 @@ class Session():
     def get_session(self):
         return self.session
 
+
 class GenEngine:
-    """ abstract defining connections here. Local assumes a psql instance on the metal. """     
-    def __init__(self, env: str, local: bool = False ) -> None:
+    """ abstract defining connections here. Local assumes a psql instance on the metal. """
+
+    def __init__(self, env: str, local: bool = False) -> None:
         if local:
             self.engine = self._local_engine()
         else:
@@ -30,17 +32,17 @@ class GenEngine:
         return self.engine
 
     def _local_engine(self) -> engine.base.Engine:
-            return create_engine('postgresql://configurator:configurator@localhost/configuration_application')
+        return create_engine('postgresql://configurator:configurator@localhost/configuration_application')
 
     def _secret_defined_engine(self) -> engine.base.Engine:
-            ##TODO: in DC-57 update this to use secret
-            pass
-   
-    
+        # TODO: in DC-57 update this to use secret
+        pass
+
 
 """Mixins
     Inheritance classes to supply standardized columns.
 """
+
 
 class UniversalMixin:
     created_at = Column(TIMESTAMP(timezone=True),
@@ -48,6 +50,7 @@ class UniversalMixin:
     updated_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=func.now())
     last_actor = Column(String)
+
 
 class UniversalWithPrimary(UniversalMixin):
     id = Column(Integer, primary_key=True)
@@ -59,20 +62,18 @@ Each of these classes represents an entity in the database.
 Logic for updates and audits will be handled via migrations. 
 """
 
-class PharmaceuticalCompany(UniversalWithPrimary, Base):
-    __tablename__ = 'pharmaceutical_companies'
-    name = Column(String, nullable=False)
-    display_name = Column(String, nullable=False)
-    brands = relationship("Brand", back_populates="pharmaceutical_company")
-       
+
 class Brand(UniversalWithPrimary, Base):
     __tablename__ = 'brands'
     name = Column(String, nullable=False)
     display_name = Column(String, nullable=False)
-    pharmaceutical_company_id = Column(Integer, ForeignKey('pharmaceutical_companies.id'), nullable = False)
-    pharmaceutical_company = relationship("PharmaceuticalCompany", back_populates='brands') 
+    pharmaceutical_company_id = Column(Integer, ForeignKey(
+        'pharmaceutical_companies.id'), nullable=False)
+    pharmaceutical_company = relationship(
+        "PharmaceuticalCompany", back_populates='brands')
     pipelines = relationship("Pipeline", back_populates='brand')
-    
+
+
 class ExtractConfiguration(UniversalWithPrimary, Base):
     __tablename__ = 'extract_configurations'
     transformation_id = Column(Integer, ForeignKey(
@@ -80,56 +81,70 @@ class ExtractConfiguration(UniversalWithPrimary, Base):
     filesystem_path = Column(String)
     prefix = Column(String)
     secret_name = Column(String, nullable=False)
-    transformation = relationship("Transformation", back_populates='extract_configurations')
+    transformation = relationship(
+        "Transformation", back_populates='extract_configurations')
 
 
-class Transformation(UniversalWithPrimary, Base):
-    __tablename__ = 'transformations'
-    transformation_template_id = Column(Integer,  ForeignKey('transformation_templates.id'), nullable = False)
-    transformation_template = relationship("TransformationTemplate")
-    pipeline_state_id = Column(Integer,  ForeignKey('pipeline_states.id'), nullable = False)
-    pipeline_state = relationship("PipelineState")
-    graph_order = Column(Integer, nullable=False, server_default=text('0'))
-    extract_configurations = relationship(
-        "ExtractConfiguration", order_by=ExtractConfiguration.id, back_populates='transformation')
-
-class PipelineState(UniversalWithPrimary, Base):
-    __tablename__ = 'pipeline_states'
-    pipeline_state_type_id = Column(Integer, ForeignKey('pipeline_state_types.id'), nullable = False)
-    pipeline_state_type = relationship("PipelineStateType")
-    pipeline_id = Column(Integer, ForeignKey('pipelines.id'), nullable = False)
-    pipeline = relationship("Pipeline", back_populates="pipeline_states")
-    graph_order = Column(Integer, nullable = False)
-    
-class PipelineStateType(UniversalWithPrimary, Base):
-    __tablename__ = 'pipeline_state_types'
+class PharmaceuticalCompany(UniversalWithPrimary, Base):
+    __tablename__ = 'pharmaceutical_companies'
     name = Column(String, nullable=False)
+    display_name = Column(String, nullable=False)
+    brands = relationship("Brand", back_populates='pharmaceutical_company')
 
 
 class Pipeline(UniversalWithPrimary, Base):
     __tablename__ = 'pipelines'
     name = Column(String, nullable=False)
-    pipeline_type_id = Column(Integer, ForeignKey('pipeline_types.id'), nullable = False)
+    pipeline_type_id = Column(Integer, ForeignKey(
+        'pipeline_types.id'), nullable=False)
     pipeline_type = relationship("PipelineType")
-    brand_id =  Column(Integer, ForeignKey('brands.id'), nullable = False)
+    brand_id = Column(Integer, ForeignKey('brands.id'), nullable=False)
     brand = relationship("Brand", back_populates="pipelines")
     run_frequency = Column(String)
     pipeline_states = relationship("PipelineState", back_populates='pipeline')
 
+
+class PipelineState(UniversalWithPrimary, Base):
+    __tablename__ = 'pipeline_states'
+    pipeline_state_type_id = Column(Integer, ForeignKey(
+        'pipeline_state_types.id'), nullable=False)
+    pipeline_state_type = relationship("PipelineStateType")
+    pipeline_id = Column(Integer, ForeignKey('pipelines.id'), nullable=False)
+    pipeline = relationship("Pipeline", back_populates="pipeline_states")
+    graph_order = Column(Integer, nullable=False)
+
+
+class PipelineStateType(UniversalWithPrimary, Base):
+    __tablename__ = 'pipeline_state_types'
+    name = Column(String, nullable=False)
+
+
 class PipelineType(UniversalWithPrimary, Base):
     __tablename__ = 'pipeline_types'
     name = Column(String, nullable=False)
-    segment_id = Column(Integer, ForeignKey('segments.id'), nullable = False)
+    segment_id = Column(Integer, ForeignKey('segments.id'), nullable=False)
     segment = relationship("Segment", back_populates='pipeline_types')
+
 
 class Segment(UniversalWithPrimary, Base):
     __tablename__ = 'segments'
     name = Column(String, nullable=False)
     pipeline_types = relationship("PipelineType", back_populates='segment')
-   
+
+
+class Transformation(UniversalWithPrimary, Base):
+    __tablename__ = 'transformations'
+    transformation_template_id = Column(Integer,  ForeignKey(
+        'transformation_templates.id'), nullable=False)
+    transformation_template = relationship("TransformationTemplate")
+    pipeline_state_id = Column(Integer,  ForeignKey(
+        'pipeline_states.id'), nullable=False)
+    pipeline_state = relationship("PipelineState")
+    graph_order = Column(Integer, nullable=False, server_default=text('0'))
+    extract_configurations = relationship(
+        "ExtractConfiguration", order_by=ExtractConfiguration.id, back_populates='transformation')
+
+
 class TransformationTemplate(UniversalWithPrimary, Base):
     __tablename__ = 'transformation_templates'
     name = Column(String, nullable=False)
-    
-
-
