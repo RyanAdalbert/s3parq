e2ee96449983cf6a46aa5e2b1b3f0710e71c0617
Ethan Knox <emk@integrichain.com>
2019-01-21 10:29:57 -0500

    cleanup

--- database/20733536743b905526ddf7f8d48073509e7afb16_down.sql
+++
@@ -1,5 +0,0 @@
-/*  DOWN!   */
-DROP FUNCTION public.trigger_updated_at_timestamp CASCADE;
-DROP FUNCTION public.audit_events CASCADE;
-DROP FUNCTION public.delete_alt CASCADE;
-DROP SCHEMA auditing CASCADE;
