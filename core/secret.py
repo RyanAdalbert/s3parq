1bdefbeb3c95453b72d2f69008636738394d5b1e
Ethan Knox <emk@integrichain.com>
2019-01-22 17:03:57 -0500

    Tests pass on the metal. Tests fail in Docker, for some reason not mocking and actually calling aws.

--- core/secret.py
+++ core/secret.py
@@ -1,44 +1,69 @@
 import boto3
-
+import json
 
 
 class Secret:
     
-    def __init__(self, name: str = None, env: str = None, type_of: str = None, identifier: str = None, force_env: bool = False) -> None:
+    def __init__(self, name: str = None, env: str = None, type_of: str = None, mode: str = None, identifier: str = None, force_env: bool = False) -> None:
         ''' get the secret from secrets manager based on args.
             ARGS:
                 - name (str): this is the human-readable name, also middle part of the fully formed secret contract
                 - env (str): one of dev, prod, uat, all
                 - type_of (str): one of FTP, database, s3
+                - mode (str): one of read / write 
                 - identifier (str): if defined this is the fully formed secret contract exactly as it is in secrets manager
                 - force_env (bool): if True, do not sub the universal secret (ie "all") when the given env is not found
         ''' 
-        self.client = boto3.client('secretsmanager','us-east-1')    
+        self.client = boto3.client('secretsmanager')
         if identifier:
-            raw_secret = self._get_secret(identifier, force_env)
-        elif name:
-            raw_secret = self._get_secret(f'{env}/{type_of}/{name}/{mode}', force_env)
+            self._parse_identifier(identifier)  
+            self.identifier = identifier
         else:
-            raise ValueError('Secret requires either a name or identifier argument')
+            self.name = name
+            self.environment = env
+            self.type_of = type_of 
+            self.mode = mode
+            self.identifier = self._build_identifier(name=name, env=env, type_of=type_of, mode=mode)
         
-        
-    
+        raw_secret = self._get_secret(self.identifier, force_env)
 
-        
-    def _get_secret(self, identifier: str) -> str:
+        self._parse_secret(json.loads(raw_secret['SecretString']))
+
+
+     
+    def _parse_identifier(self, identifier: str) -> None:
+        try:
+            identifier_parts = identifier.split('/')
+            self.environment, self.type_of, self.name, self.mode = identifier_parts
+        except:
+            raise ValueError(f'{identifier} is not a valid secret identifier')     
+
+    def _build_identifier(self, env: str, type_of: str, name: str, mode: str) -> str: 
+        return f'{env}/{type_of}/{name}/{mode}'        
+
+    def _get_secret(self, identifier: str, force_env: bool) -> str:
         # first look to see if the explicit secret exists
         try:
-            raw_secret = self.client.get_secret_value(identifier)
-        except Exception, e:
+            raw_secret = self.client.get_secret_value(SecretId=identifier)
+        except Exception as e:
             if not force_env:
                 # look for a universal secret with the same signature
                 try:
-                    raw_secret = self.client.get_secret_value('all' + ('/'.join(identifier.split('/')[1:])))
+                    raw_secret = self.client.get_secret_value(SecretId='all/' + ('/'.join(identifier.split('/')[1:])))
                 except:
                     raise e
             else:
                 raise e 
         return raw_secret
             
-       
-       
+    def _parse_secret(self, secret: dict) -> None:      
+        if self.type_of == 'FTP':
+            for val in ('user','password','host','method'):
+                self.__dict__[val] = secret.get(val,None)
+            self.connection_mode = secret['mode']
+            self.port = int(secret.get('port',None))
+
+        elif self.type_of == 'database':
+            for val in ('user', 'password', 'rdbms', 'schema', 'role','host','database'):
+                self.__dict__[val] = secret.get(val,None)
+                self.port = int(secret.get('port',None))
--- tests/test_secret.py
+++ tests/test_secret.py
@@ -1,61 +1,64 @@
 import pytest
-import boto3
 from moto import mock_secretsmanager
+import boto3
 from core.secret import Secret
+import json
 
-@pytest.fixture
-def setup_secretsmanager_mock():
-    mock = mock_secretsmanager()
-    mock.start()
-    
+@mock_secretsmanager
+class Test:
+    def setup(self):
+        secret_client = boto3.client('secretsmanager')
+        ftp_secret_string = '{"user":"GOB_Bluth", "password":"S3@L_s@le", "host":"ftp://bluth.com", "port":"22", "mode":"active","method":"SFTP"}'
+        mysql_secret_string = '{"user":"ic_db_user", "password":"uns3cur3", "host":"12345-host.aws.integrichain.net", "port":"3306", "database": "bluth", "rdbms":"mysql"}'
+        psql_read_secret_string = '{"user":"ic_db_user2", "password":"uns3cur3", "host":"22222-host.aws.integrichain.net", "port":"5432", "database": "configuration_application", "rdbms":"postgres", "schema":"public", "role":"public"}'
+        psql_write_secret_string = '{"user":"ic_db_user2", "password":"uns3cur3", "host":"22222-host.aws.integrichain.net", "port":"5432", "database": "configuration_application", "rdbms":"postgres", "schema":"public", "role":"configurator"}'
+        secret_client.create_secret(  Name = 'all/FTP/bluth/read',
+                                    Description = 'FTP connection for bluth source data.',
+                                    SecretString = ftp_secret_string
+                                 ) 
 
-@pytest.fixture
-def seed_mock_secrets():
-    ftp_secret_string = '{"username":"GOB_Bluth", "password":"S3@L_s@le", "host":"ftp://bluth.com", "port":"22", "mode":"active","method":"SFTP"}'
-    mysql_secret_string = '{"username":"ic_db_user", "password":"uns3cur3", "host":"12345-host.aws.integrichain.net", "port":"3306", "database": "bluth", "rdbms":"mysql"}'
-    psql_read_secret_string = '{"username":"ic_db_user2", "password":"uns3cur3", "host":"22222-host.aws.integrichain.net", "port":"5432", "database": "configuration_application", "rdbms":"postgres", "schema":"public", "role":"public"}'
-    psql_write_secret_string = '{"username":"ic_db_user2", "password":"uns3cur3", "host":"22222-host.aws.integrichain.net", "port":"5432", "database": "configuration_application", "rdbms":"postgres", "schema":"public", "role":"configurator"}'
+        secret_client.create_secret(  Name = 'prod/database/bluth/write',
+                                    Description = 'Intermediary publish table for bluth transformed data.',
+                                    SecretString = mysql_secret_string
+                                 ) 
 
-    mock_secret = boto3.client('secretsmanager', region_name = 'us-east-1')
-    
-    mock_secret.create_secret(  Name = 'all/FTP/bluth/read',
-                                Description = 'FTP connection for bluth source data.',
-                                SecretString = ftp_secret_string
-                             ) 
+        secret_client.create_secret(  Name = 'all/database/configuration_application/read',
+                                    Description = 'Read connection for the production configuration database.',
+                                    SecretString = psql_read_secret_string
+                                 ) 
 
-    mock_secret.create_secret(  Name = 'prod/database/bluth/write',
-                                Description = 'Intermediary publish table for bluth transformed data.',
-                                SecretString = mysql_secret_string
-                             ) 
+        secret_client.create_secret(  Name = 'prod/database/configuration_application/write',
+                                    Description = 'Write connection for the production configuration database.',
+                                    SecretString = psql_write_secret_string
+                                 ) 
 
-    mock_secret.create_secret(  Name = 'all/database/configuration_application/read',
-                                Description = 'Read connection for the production configuration database.',
-                                SecretString = psql_read_secret_string
-                             ) 
+    def test_get_secret_with_args(self):        
+        self.setup()
+        secret = Secret(name='bluth',
+                        env='all',
+                        type_of='FTP',
+                        mode='read'
+                        )
 
-    mock_secret.create_secret(  Name = 'prod/database/configuration_application/write',
-                                Description = 'Write connection for the production configuration database.',
-                                SecretString = psql_write_secret_string
-                             ) 
+        assert secret.identifier == 'all/FTP/bluth/read'
+        assert secret.password == "S3@L_s@le"
 
+    def test_get_secret_with_string(self):
+        self.setup()
+        secret = Secret(identifier='prod/database/bluth/write')
+        assert secret.name == 'bluth'
+        assert secret.password == 'uns3cur3'
 
-def test_get_secret_with_args():
-    secret = Secret(common_name='bluth',
-                    env='dev',
-                    type_of='FTP'
-                    )
+    def test_none_empty_vals(self):
+        self.setup()
+        secret = Secret(name='bluth',
+                        env='prod',
+                        type_of='database',
+                        mode='write')
 
-    assert secret.name == 'all/FTP/bluth/read'
-    assert secret.password == "S3@L_s@le"
+        assert secret.schema == None
 
-def test_get_secret_with_string():
-    secret = Secret(name='prod/database/bluth/write')
-    assert secret.common_name == 'bluth'
-    assert secret.password == 'uns3cur3'
-
-def test_none_empty_vals():
-    secret = Secret(common_name='bluth',
-                    env='all',
-                    type_of='FTP')
-
-    assert secret.schema == None
+    def test_env_sub(self):
+        self.setup()
+        secret = Secret(name='configuration_application', env='dev', type_of='database', mode='read')
+        assert secret.role == 'public'
