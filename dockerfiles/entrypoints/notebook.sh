1075acbe851de851dd794d2db348d2642df71134
Nate Sanford <nbs@integrichain.com>
2019-02-01 11:39:34 -0500

    clean up notebook configuration

--- dockerfiles/entrypoints/notebook.sh
+++ dockerfiles/entrypoints/notebook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-jupyter notebook --config /notebook/jupyter_notebook_config.py --allow-root --port 8888 --NotebookApp.token=''
+jupyter notebook --config /notebook/jupyter_notebook_config.py
