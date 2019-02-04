f71a3129e96ec54752f267def9374dfb023d0085
Rayne N. Schaffer <rns@integrichain.com>
2019-02-04 09:47:50 -0500

    added cli to setup and added basic test

--- corebot/cli.py
+++ corebot/cli.py
@@ -6,12 +6,13 @@ from core.helpers import notebook
 def cli(): # pragma: no cover
     pass
 
+# Adjusting locked env variable from local -> dev for proper run, until env vars are adjusted per DC-157
 @cli.command()
-@click.argument('env', type=click.Choice(['local']))
+@click.argument('env', type=click.Choice(['dev']))
 @click.argument('id', type=int)
 @click.argument('input_contract', type=str)
 @click.argument('output_contract', type=str)
 def run(env, id, input_contract, output_contract):
-    notebook_url = notebook.run_transform("dev", id, input_contract, output_contract)
+    notebook_url = notebook.run_transform(env, id, input_contract, output_contract)
     print("See your notebook output at:")
     print(notebook_url)
