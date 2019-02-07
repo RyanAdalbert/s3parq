67e0c3921a0ab6d7e4ec07e5ffa611a455881cdb
Ethan Knox <emk@integrichain.com>
2019-02-07 15:07:23 -0500

    logging, cleanup

--- core/airflow/dagbuilder/dag_builder.py
+++ core/airflow/dagbuilder/dag_builder.py
@@ -3,13 +3,12 @@ from core.models.configuration import Pipeline
 from core.helpers.session_helper import SessionHelper
 from datetime import datetime, timedelta
 from core.airflow.dagbuilder.task_orchestrator import TaskOrchestrator
+from core.logging import LoggerMixin
 
-class DagBuilder:
+class DagBuilder(LoggerMixin):
 
-       
-    def do_build_dags(self)->None:
-        """Integrates all the components of getting dags, setting task deps etc."""
-
+    def __init__(self):
+        self._dags = []
         self.DEFAULT_ARGS = {
         "owner": "integriChain",
         "depends_on_past": False,
@@ -20,7 +19,12 @@ class DagBuilder:
         "retries": 1,
         "retry_delay": timedelta(minutes=5)
         }
-        self._dags = []
+        
+       
+    def do_build_dags(self)->None:
+        """Integrates all the components of getting dags, setting task deps etc."""
+        self.logger.info('Beginning run to build all dags...')
+
         self._pipelines = self._get_pipelines()
         sets = self._create_dag_sets(self._pipelines)
         for pipeline, dag in sets:
@@ -41,12 +45,15 @@ class DagBuilder:
                 - the original pipeline object
                 - the matching DAG for that pipeline"""
         dags = []
+        self.logger.debug("Creating DAGs from pipelines...")
         for pipe in pipelines:
-            
+            self.logger.debug(f"Created DAG {pipe.name}")
             dags.append((pipe, DAG( pipe.name, 
                                     default_args = self.DEFAULT_ARGS,
-                                    start_date=datetime(2000, 1, 1), 
+                                    ## airflow may no longer support start_date in default_args
+                                    start_date = datetime(2000, 1, 1),
                                     schedule_interval=f'@{pipe.run_frequency}'),))
+            self.logger.debug("Done creating DAGs.")
         return dags                     
 
 
@@ -56,13 +63,14 @@ class DagBuilder:
                 - only_active: if true ignore inactive pipelines
             RETURNS list of core.model.Pipeline objects
         """
+        self.logger.debug(f"getting {'active' if only_active else 'all'} pipelines from config...")
         session = SessionHelper().session
 
         if only_active:
             pipelines = session.query(Pipeline).filter(Pipeline.is_active)
         else:
             pipelines = session.query(Pipeline)
-        
+        self.logger.debug(f"Done getting pipelines, {len([x for x in pipelines])} pipelines found.")
         return pipelines
 
     def _get_prepped_tasks(self, pipeline: Pipeline, dag: DAG)-> tuple:
