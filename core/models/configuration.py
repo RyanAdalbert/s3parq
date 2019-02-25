4847c306ad4f0d9b4879e3a1eb4ded90c6c83b32
Ethan Knox <emk@integrichain.com>
2019-02-25 20:19:28 +0000

    fixed dev => uat shims

--- core/models/configuration.py
+++ core/models/configuration.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import session, sessionmaker, relationship
 from core.constants import DEV_CONFIGURATION_APPLICATION_CONN_STRING, ENVIRONMENT
 from core.secret import Secret
+from core.logging import LoggerMixin
 Base = declarative_base()
 
 
@@ -19,7 +20,7 @@ class Session():
     def get_session(self) -> session.Session:
         return self.session
 
-class GenerateEngine:
+class GenerateEngine(LoggerMixin):
     """ abstract defining connections here. Local assumes a psql instance in a local docker container. """
 
     def __init__(self, in_memory:bool=True) -> None:
@@ -52,10 +53,10 @@ class GenerateEngine:
                         mode='read'
                         )
         if secret.rdbms == "postgres":
-            conn_string = f"postgresql://{secret.user}:{secret.password}@{host}/{database}"
+            conn_string = f"postgresql://{secret.user}:{secret.password}@{secret.host}/{secret.database}"
         else:
             m = "Only postgres databases are supported for configuration_application at this time."
-            logger.critical(m)
+            self.logger.critical(m)
             raise NotImplementedError(m)
         return conn_string
 
