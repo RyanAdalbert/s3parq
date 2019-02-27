aae2b151c27c3f4f870235e372f8772e89ac26eb
Ethan Knox <emk@integrichain.com>
2019-02-27 09:06:26 -0500

    corrected seed flip-flop of cols and bi naming

--- seeds/2019.02.13__14.02.sql
+++ seeds/2019.02.13__14.02.sql
@@ -77,7 +77,7 @@ INSERT INTO transformations (transformation_template_id, pipeline_state_id, grap
         ((SELECT id FROM transformation_templates WHERE name = 'extract_from_ftp'),(SELECT id FROM pipeline_states WHERE pipeline_state_type_id = (SELECT id FROM pipeline_state_types WHERE name = 'raw') AND pipeline_id = (SELECT id FROM pipelines WHERE name = 'alkermes_just_extract')),0, 'emk@integrichain.com'),
         ((SELECT id FROM transformation_templates WHERE name = 'extract_from_ftp'),(SELECT id FROM pipeline_states WHERE pipeline_state_type_id = (SELECT id FROM pipeline_state_types WHERE name = 'raw') AND pipeline_id = (SELECT id FROM pipelines WHERE name = 'bi_just_extract')),0, 'emk@integrichain.com');
 
-INSERT INTO extract_configurations (transformation_id, secret_name, secret_type_of, prefix, filesystem_path, last_actor)
+INSERT INTO extract_configurations (transformation_id, secret_name, secret_type_of, filesystem_path, prefix, last_actor)
     VALUES
         ((SELECT id FROM transformations WHERE transformation_template_id = (SELECT id FROM transformation_templates WHERE name = 'extract_from_ftp') AND pipeline_state_id = (SELECT id FROM pipeline_states WHERE pipeline_state_type_id = (SELECT id FROM pipeline_state_types WHERE name = 'raw') AND pipeline_id = (SELECT id FROM pipelines WHERE name = 'sun_just_extract'))), 
         'sun',
@@ -86,13 +86,13 @@ INSERT INTO extract_configurations (transformation_id, secret_name, secret_type_
         'INTEGRICHAIN_SUN_',
         'emk@integrichain.com'),
         ((SELECT id FROM transformations WHERE transformation_template_id = (SELECT id FROM transformation_templates WHERE name = 'extract_from_ftp') AND pipeline_state_id = (SELECT id FROM pipeline_states WHERE pipeline_state_type_id = (SELECT id FROM pipeline_state_types WHERE name = 'raw') AND pipeline_id = (SELECT id FROM pipelines WHERE name = 'bi_just_extract'))), 
-        'bi_ipf',
+        'bi',
         'ftp',
         'SpecilatyAnalytics',
         'IPF_PAT_',
         'emk@integrichain.com'),
         ((SELECT id FROM transformations WHERE transformation_template_id = (SELECT id FROM transformation_templates WHERE name = 'extract_from_ftp') AND pipeline_state_id = (SELECT id FROM pipeline_states WHERE pipeline_state_type_id = (SELECT id FROM pipeline_state_types WHERE name = 'raw') AND pipeline_id = (SELECT id FROM pipelines WHERE name = 'bi_just_extract'))), 
-        'bi_bridge',
+        'bi',
         'ftp',
         'SpecilatyAnalytics',
         'BRIDGE_PAT_',
