644d10ce5cc50bfd2417a03b55cb2be664806412
Natie J. Bohnel <bohnenj@NJB-MAC.local>
2019-04-15 11:10:46 -0400

    added basic json serialization for sql objects

--- core/api/app.py
+++ core/api/app.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python
-from flask import Flask
-#from core.constants import BRANCH_NAME
+from datetime import datetime
+import json
+from flask import Flask, request
+from core.helpers.configuration_mocker import ConfigurationMocker as CMock
+from core.models.configuration import (
+    PharmaceuticalCompany, 
+    Brand, 
+    Pipeline, 
+    PipelineType, 
+    Segment
+)
 
-app = Flask(__name__)
+def jsonify(self): # takes a defined db data class and serializes it into json
+    data = self.__dict__.copy()
+    del data['_sa_instance_state']
+    for key, value in data.items():
+        if(isinstance(value, datetime)):
+            data[key] = value.strftime("%m/%d/%Y %H:%M")
+    return json.dumps(data)
+
+def queryjson(self):
+    jstring = "{\n"
+    for obj in query:
+        key = obj.id
+        val = jsonify(obj)
+        jstring += "\"" + key + "\":" + val + ",\n"
+
+mocker = CMock()
+mocker.generate_mocks()
+session = mocker.get_session()
+
+query = session.query(Brand).all()
 
 try:
     from core.constants import BRANCH_NAME
@@ -10,8 +38,20 @@ try:
 except Exception as e:
     b = e
 
-@app.route('/')
-def hello_world():
-    return f"hello CORE world! {b}"
+app = Flask(__name__)
+#app.register_blueprint(routes)
+with app.app_context():
+    print(jsonify(query))
+
+@app.route('/api')
+def index():
+    return "No request specified. Did you mean /api/login?"
+
+@app.route('/api/login')
+def login():
+    if request.method != 'POST':
+        return "Error: Login information not specified."
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
