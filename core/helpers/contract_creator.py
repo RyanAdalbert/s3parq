93d918a683be653bc004486f9af767c18d174ae2
natie <njb@integrichain.com>
2019-05-02 12:07:22 -0400

    Moved get_relative_contract (formerly contract_from_transformation_name) back into contract_creator helper module for external use

--- core/helpers/contract_creator.py
+++ core/helpers/contract_creator.py
@@ -21,3 +21,14 @@ def contract_from_transformation_id(t_id: int)->DatasetContract:
     state = transform.pipeline_state.pipeline_state_type.name
     child = transform.pipeline_state.pipeline.brand.name
     return DatasetContract(parent=parent, child=child, state=state, dataset=dataset)
+
+# Returns a contract corresponding to a specified transformation name relative to an existing contract.
+# The parent/child (pharma/brand) info for the returned contract is populated from the extant contract, typically the one used to construct a DatasetDiff.
+def get_relative_contract(t_name: str, contract:DatasetContract)->DatasetContract:
+    sess = SHelp().session
+    template = sess.query(TransformationTemplate).filter(TransformationTemplate.name==t_name).first()
+    if template is None:
+        raise KeyError("Error: No transform found with name " + t_name)
+    dataset = template.name
+    state = template.pipeline_state_type.name 
+    return DatasetContract(parent=contract.parent, child=contract.child, state=state, dataset=dataset) 
