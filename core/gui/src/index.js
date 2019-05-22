fa02dbe8f2bffe0979f055f5811d29c63740c7a4
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-05-22 14:04:35 -0400

    renamed configureStore file to store

--- core/gui/src/index.js
+++ core/gui/src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './index.css';
 import App from './app/App';
-import store from './redux/configureStore';
+import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
