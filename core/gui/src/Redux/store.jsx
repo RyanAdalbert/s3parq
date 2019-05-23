a6873c9d83cfa3e047131861d7454fac0c4824ef
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-05-23 10:50:39 -0400

    added a few comments to store

--- core/gui/src/Redux/store.jsx
+++ core/gui/src/Redux/store.jsx
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
+//our load state function
 const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state');
@@ -16,6 +17,7 @@ const loadState = () => {
   }
 };
 
+//save our state to localstorage
 export const saveState = state => {
   try {
     const serializedState = JSON.stringify(state);
@@ -27,6 +29,7 @@ export const saveState = state => {
 
 const persistedState = loadState();
 
+//set up our store with the redux chrome extension
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
