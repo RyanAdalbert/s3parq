5da1732005315ab1fe7cb818133ab16114614da9
Justin T. Lewis <lewisjt@JTL-MAC.local>
2019-06-05 14:56:33 -0400

    Set up some basic tests for rendering of components.

--- core/gui/src/components/Pipelines/Pipelines.test.jsx
+++ core/gui/src/components/Pipelines/Pipelines.test.jsx
@@ -1,1 +1,8 @@
-import React from 'react';
+import Pipelines from './Pipelines';
+
+describe('<Pipelines /> rendering', () => {
+  const wrapper = shallow(<Pipelines />);
+  it('should render a <TableContainer /> to display the pipelines', () => {
+    expect(wrapper.find(TableContainer)).toHaveLength(1);
+  });
+});
