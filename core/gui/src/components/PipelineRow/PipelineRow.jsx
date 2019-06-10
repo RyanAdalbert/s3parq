d541c9e941ccf1ac91b015120abf416a1ed6e586
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-06-10 11:41:40 -0300

    started setting up the action creators for the modals, and the modal components

--- core/gui/src/components/PipelineRow/PipelineRow.jsx
+++ core/gui/src/components/PipelineRow/PipelineRow.jsx
@@ -1,28 +1,38 @@
 import React, { PureComponent } from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
-import multiFilter from '../../utils/multiFilter/multiFilter';
 
+import multiFilter from '../../utils/multiFilter/multiFilter';
+import guiModal from '../guiModal/guiModal';
+import { modalToggle } from '../../redux/actions/pipelineActions/pipelineActions';
 //Pipeline row component
 export default class PipelineRow extends PureComponent {
   render() {
-    const { pipelines } = this.props;
-    const { setFilters } = this.props;
+    const { pipelines, setFilters, dispatch } = this.props;
 
     const filtered = multiFilter(pipelines, setFilters);
 
+    let modalClose = () => this.setState({ modalShow: false });
+
     const key = Object.keys(pipelines);
+
+    const handleClick = modalState => {
+      dispatch(modalToggle(modalState));
+    };
+
     const pipelineRow = filtered.map(pipeline => {
       return (
         // creates modals
-        <tr key={key + pipeline.name}>
-          <td>{key}</td>
-          <td>{pipeline.name}</td>
-          <td>{pipeline.brand}</td>
-          <td>{pipeline.pharma_company}</td>
-          <td>{pipeline.status}</td>
-          <td>{pipeline.run_freq}</td>
-        </tr>
+        <>
+          <tr key={key + pipeline.name} onClick={() => handleClick(true)}>
+            <td>{key}</td>
+            <td>{pipeline.name}</td>
+            <td>{pipeline.brand}</td>
+            <td>{pipeline.pharma_company}</td>
+            <td>{pipeline.status}</td>
+            <td>{pipeline.run_freq}</td>
+          </tr>
+
+          <guiModal show={this.props.modalShow} onHide={modalClose} />
+        </>
       );
     });
 
