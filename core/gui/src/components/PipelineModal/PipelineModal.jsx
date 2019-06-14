6c68cf6234d6df5109e14a677ea4dbb0099faf43
Justin T. Lewis <lewisjt@JTL-MAC.local>
2019-06-14 11:21:01 -0400

    Added pipeline info to the modal

--- core/gui/src/components/PipelineModal/PipelineModal.jsx
+++ core/gui/src/components/PipelineModal/PipelineModal.jsx
@@ -16,13 +16,32 @@ class PipelineModal extends React.Component {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            {this.props.pipelineInfo.name}
+            {this.props.pipelineInfo.brand} : {this.props.pipelineInfo.name}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>hi</h4>
-          <p>hi</p>
-          <h3>hi</h3>
+          <h4>Description</h4>
+          <p>{this.props.pipelineInfo.description}</p>
+          <h3>Type</h3>
+          <p>{this.props.pipelineInfo.type}</p>
+          <h3>States</h3>
+          <div>
+            <ul>
+              {this.props.pipelineInfo.states.map((stateInfo, index) => (
+                <li key={index}>{stateInfo}</li>
+              ))}
+            </ul>
+          </div>
+          <h3>Transforms</h3>
+          <div>
+            <ul>
+              {this.props.pipelineInfo.transformations.map(
+                (transform, index) => (
+                  <li key={index}>{transform}</li>
+                )
+              )}
+            </ul>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
