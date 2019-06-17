1c138217935f7fc743e32259cceb30c965ee0f2e
natie <njb@integrichain.com>
2019-06-17 11:58:25 -0400

    fixed merge conflicts & removed notebook run metadata

--- seeds/2019.04.15__14.19.sql
+++ seeds/2019.04.15__14.19.sql
@@ -1,12 +1,13 @@
 BEGIN;
 INSERT INTO 
     ADMINISTRATORS (email_address, first_name, last_name)
-VALUES
-    ('epd@integrichain.com','Ed','Doran'),
-    ('mgg@integrichain.com','Miguel','Guerreiro'),
-    ('rns@integrichain.com','Rayne','Schaffer'),
-    ('emk@integrichain.com','Ethan','Knox'),
-    ('njb@integrichain.com','Natie','Bohnel'),
-    ('ajw@integrichain.com','Alec','Wertheimer'),
-    ('jlewis@integrichain.com', 'Justin', 'Lewis'),
-    ('jtobias@integrichain.com', 'Jacob', 'Tobias');
+    VALUES
+        ('epd@integrichain.com','Ed','Doran'),
+        ('mgg@integrichain.com','Miguel','Guerreiro'),
+        ('rns@integrichain.com','Rayne','Schaffer'),
+        ('emk@integrichain.com','Ethan','Knox'),
+        ('njb@integrichain.com','Natie','Bohnel'),
+        ('ajw@integrichain.com','Alec','Wertheimer'),
+        ('jlewis@integrichain.com', 'Justin', 'Lewis'),
+        ('jtobias@integrichain.com', 'Jacob', 'Tobias');
+COMMIT;
