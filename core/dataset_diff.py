8f28ea54e5416dc8260c9c406bbe9dab149ab559
natie <njb@integrichain.com>
2019-06-18 13:34:55 -0400

    reordered kwargs

--- core/dataset_diff.py
+++ core/dataset_diff.py
@@ -8,7 +8,7 @@ class DatasetDiff():
         self.contract = contract_creator.contract_from_transformation_id(t_id=transform_id)
         return
     
-    def get_diff(self, transform_name: str, partition="__metadata_run_id":str, part_vals:List[any], comparison="==":str)->pd.DataFrame:
+    def get_diff(self, transform_name: str, part_vals:List[any], partition="__metadata_run_id":str, comparison="==":str)->pd.DataFrame:
         if not hasattr(self, 'contract') or type(self.contract) != DatasetContract:
             raise NameError("No source contract set. Did you pass a valid transform id when creating the class?")
         input_contract = contract_creator.get_relative_contract(t_name=transform_name, contract=self.contract)
