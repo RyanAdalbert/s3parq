a5e244e3eeb99b143d5a47dc74d9efa815917c5b
Jacob S. Tobias <tobiajs@JST-MAC.local>
2019-06-24 16:15:08 -0400

    Add test making sure xcomm_push gets called with the proper values

---
+++ tests/unit/test_runevent.py
@@ -0,0 +1,32 @@
+import pytest
+import mock
+from unittest.mock import MagicMock, patch
+from core.airflow.plugins.runevent_operator import RunEvent_task as ret
+from core.helpers.session_helper import SessionHelper
+import core.models.configuration as config
+
+class MockSession:
+
+    def add(self, run_event_obj):
+        run_event_obj.id = 42
+
+    def commit(self):
+        return True
+
+    def close(self):
+        return True
+
+
+class MockXComm:
+
+    def xcom_push(self, **kwargs):
+        return True
+
+
+def test_xcomm_push_called_with_42(monkeypatch):
+    session_mock = MockSession()
+    xcomm_mock = MockXComm()
+    xcomm_mock.xcom_push = MagicMock()
+    monkeypatch.setattr(SessionHelper, "session", session_mock)
+    ret(1, ti=xcomm_mock)
+    xcomm_mock.xcom_push.assert_called_with(key="run_id", value=42)
