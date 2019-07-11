e43059a2f4a935cf1efaa0dc23c4750908bc3962
natie <njb@integrichain.com>
2019-07-11 15:58:16 -0400

    green light tests and an updated template

--- core/helpers/pipeline_builder.py
+++ core/helpers/pipeline_builder.py
@@ -1,28 +1,52 @@
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from core.helpers.session_helper import SessionHelper
-from core.models.configuration import Pipeline, Transformation, PipelineState
+from core.logging import LoggerSingleton
+from random import randint
+import core.helpers.postgres_toggle as pg_toggle
+import core.models.configuration as config
+import sys
 
-def __get_or_create(session:Session, model, find:dict, populate:dict):
+logger = LoggerSingleton().logger
+
+def _get_or_create(session: Session, model, find:dict):
     """ finds the model instance or creates it.
         ARGS:
-            session: the sqlaclhemy db session
+            session: SQLalchemy session
             model: the model class to look up 
             find: the key value dict to look up existing instance with
-            populate: the key values to populate a new instance with (in addition to the find values)
         RETURNS: model instace of the found/created class 
     """
     try:
-        if len(find.keys()) == 1:
-            return session.query(model).filter_by(**find).one()
-        else:
-            query = session.query(model)
-            for k,v in find.items():
-                query = query.filter_by(k=v)
-            return query.one()
-
+        return session.query(model).filter_by(**find).one()
     except NoResultFound:
-        model_args = find.update(populate)
-        new_model = model(**model_args)
+        new_model = model(**find)
         session.add(new_model)
+        session.commit()
         return new_model
+
+def build(pharma_company: str, brand: str, state: str, transformation: str, session: Session):
+    logger.debug("Adding/getting mocks for specified configurations...")
+    find = dict(name=pharma_company, display_name=pharma_company)
+    pc = _get_or_create(session, config.PharmaceuticalCompany, find)
+    find = dict(name=brand, display_name=brand, pharmaceutical_company_id=pc.id)
+    br = _get_or_create(session, config.Brand, find)
+    pipeline_name = "singleton_" + transformation
+    find = dict(name=pipeline_name, brand_id=br.id, pipeline_type_id=0)
+    pipeline = _get_or_create(session, config.Pipeline, find)
+    find = dict(name=state)
+    st_type = _get_or_create(session, config.PipelineStateType, find)
+    find = dict(pipeline_state_type_id=st_type.id, pipeline_id=pipeline.id, graph_order=0)
+    st = _get_or_create(session, config.PipelineState, find)
+    find = dict(name=transformation, pipeline_state_type_id=st_type.id)
+    tt = _get_or_create(session, config.TransformationTemplate, find)
+    find = dict(transformation_template_id=tt.id, pipeline_state_id=st.id, graph_order=0)
+    tr = _get_or_create(session, config.Transformation, find)
+    logger.debug("Done. Creating mock run event and committing results to configuration mocker.")
+    run_event = config.RunEvent(id=randint(100000, 999999), pipeline_id=pipeline.id)
+    session.add(run_event)
+    session.commit()
+    ids = [tr.id, run_event.id]
+    session.close()
+
+    return ids
---
+++ tests/unit/test_pipeline_builder.py
@@ -0,0 +1,51 @@
+import pytest
+import core.helpers.pipeline_builder as pb
+from core.helpers.session_helper import SessionHelper
+import core.models.configuration as config
+import random
+import string
+
+
+def rand_string()->str:
+    return "".join([random.choice(string.ascii_lowercase) for _ in range(random.randint(10,25))])
+
+def test_new_build():
+    session = SessionHelper().session
+    tr_id = pb.build("foo", "bar", "new_state", "new_transform", session)[0]
+    tr = session.query(config.Transformation).filter_by(id=tr_id).one()
+    assert tr.transformation_template.name == "new_transform"
+    session.close()
+
+def test_get_or_create_exists():
+    session = SessionHelper().session
+    expected = session.query(config.PharmaceuticalCompany).filter_by(id=1).one().name
+    find = dict(id=1)
+    co = pb._get_or_create(session, config.PharmaceuticalCompany, find)
+    assert co.name == expected
+    session.close()
+
+def test_get_or_create_not_exists():
+    session = SessionHelper().session
+    test_id = 1000000000
+    find = dict(id=test_id, name="bananaco", display_name="bananaco")
+    co = pb._get_or_create(session, config.PharmaceuticalCompany, find)
+    assert co.id == test_id
+    assert co.name == "bananaco"
+    session.close()
+
+"""
+def test_adds_new_pipeline():
+    pipe = dict()
+    for val in ("company","brand","dataset",):
+        pipe[val] = rand_string()
+    pipe["state"] = "ingest"
+    pipeline_id, transform_id = pb.build(company=pipe["company"],
+                                         brand=pipe["brand"],
+                                         state=pipe["state"],
+                                         dataset=pipe["dataset"])
+    session = SessionHelper().session
+    
+    t = session.query(Transform).filter(Transform.id = transform_id).one()
+    
+    assert t.pipeline_id == pipeline_id
+"""
