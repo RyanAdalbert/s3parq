8b361ca0c0b27374c798609344f497bb088ba2d3
natie <njb@integrichain.com>
2019-08-07 09:49:42 -0400

    contract changes

--- core/contract.py
+++ core/contract.py
@@ -199,10 +199,10 @@ class Contract(LoggerMixin):
     def _validate_branch(self, branch: str)->str:
         branch = branch.lower()
         protected_branches = ['master','test','rse','uat','prod']
-
-        if (self.env == self.DEV) & (branch in protected_branches):
-                raise ValueError(
-                    f'Cannot use this branch in development. Currently using branch : {branch} : when {protected_branches} are restricted branches.')
+        self.logger.debug([self.env, ENVIRONMENT, self.DEV])
+        if (self.env == self.DEV) and (branch in protected_branches):
+            raise ValueError(
+                f'Cannot use this branch in development. Currently using branch : {branch} : when {protected_branches} are restricted branches.')
         else:
             return self._validate_part(branch)
 
