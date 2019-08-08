ae656e30a9c3a158d248bb7a92c959bd8e2fd0a0
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-08-08 11:38:08 -0400

    Connects to Server and creates a contains-phi directory

--- phi-nightcrawler/nightcrawler.py
+++ phi-nightcrawler/nightcrawler.py
@@ -1,6 +1,7 @@
-import os, logging, time, shutil
+import os, logging, shutil, sys
 import paramiko
 from shutil import copyfile
+from stat import S_IWUSR, S_IWGRP, S_IWOTH
 
 sftp_info = {
     'host': os.getenv('SFTP_HOST'),
@@ -9,48 +10,30 @@ sftp_info = {
     'port': int(os.getenv('SFTP_PORT'))
 }
 
-phi_columns = os.getenv('PHI_COLUMNS')
+phi_dir = os.getenv('PHI_DIR')
 
- 
-def connect(creds):
+
+def nightcrawler(creds, new_dir):
         host = creds['host']
         user = creds['user']
         password = creds['password']
         port = creds['port']
-        remote_path = os.getenv('')
-        local_path = './test2.txt'
-
         logging.info(f"Connecting to host: {host} on port: {port}")
 
+        print('1')
         if port is None:
             transport = paramiko.Transport((host), default_window_size=paramiko.common.MAX_WINDOW_SIZE, default_max_packet_size=3276800)
         else:
             transport = paramiko.Transport((host, port), default_window_size=paramiko.common.MAX_WINDOW_SIZE, default_max_packet_size=3276800)
-    
+        print('2')
         transport.connect(username=user, password=password)
         sftp = paramiko.SFTPClient.from_transport(transport)
-
+        print('3')
         try:
-            with sftp.open(remote_path, 'rb') as remote:
-                start = time.time()
-                with open(local_path, 'wb') as local:
-                    shutil.copyfileobj(remote, local)
-                end = time.time()
-                logging.debug(f'It took {end - start} seconds to move {remote_path} to {local_path}')
+            sftp.mkdir(new_dir, mode=777)
+            print('4')
         except Exception as e: 
-            print(e)
+            logging.error(e)
+            raise e
 
-
-connect(sftp_creds)
-
-# def __run__(sftp_creds, phi_columns, directory, notification_emails):
-#         with (connect_to_server, sftp_creds) as sftp_srv:
-#             files = crawl_dir(directory)
-#             bad_dir=create_bad_files_directory(directory)
-#             for f in files:
-#                 with open('temp_file', 'rw') as temp_f:
-#                     sftp_srv.copy(f, temp_f)
-#                     has_phi=scan_for_phi(temp_f,phi_columns)
-#                     if has_phi:
-#                             move_to_bad_dir(f, bad_dir)
-#                             send_notifications(notification_emails)
+nightcrawler(sftp_info, phi_dir)
