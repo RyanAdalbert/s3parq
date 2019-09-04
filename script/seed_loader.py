56fc0e9caae1699b623d50f11a8fd3a0fd866562
zellehj <hjz@integrichain.com>
2019-09-04 09:59:55 -0400

    Actually resolved seed_loader merge conflict

--- script/seed_loader.py
+++ script/seed_loader.py
@@ -31,37 +31,12 @@ load_run_sql(seeds_dir, "administrators.sql")
 load_run_sql(seeds_dir, "raw_extract_from_ftp.sql")
 load_run_sql(seeds_dir, "initial_ingest.sql")
 load_run_sql(seeds_dir, "dispense_ingest_column_mapping.sql")
-<<<<<<< HEAD
-load_run_sql(seeds_dir, "patient_status_standardize_dates_template.sql")
-load_run_sql(seeds_dir, "patient_status_standardize_dates.sql")
-load_run_sql(seeds_dir, "patient_status_ingest_standardize_numbers_template.sql")
-load_run_sql(seeds_dir, "patient_status_ingest_standardize_numbers.sql")
-load_run_sql(seeds_dir, "patient_status_fill_rate_template.sql")
-load_run_sql(seeds_dir, "patient_status_fill_rate.sql")
-load_run_sql(seeds_dir, "enrich_pending_too_long_template.sql")
-load_run_sql(seeds_dir, "enrich_pending_too_long.sql")
-load_run_sql(seeds_dir, "patient_status_master_patient_status_template.sql")
-load_run_sql(seeds_dir, "patient_status_master_patient_status.sql")
-load_run_sql(seeds_dir, "patient_status_master_patient_status_variables.sql")
-load_run_sql(seeds_dir, "patient_status_master_patient_substatus_template.sql")
-load_run_sql(seeds_dir, "patient_status_master_patient_substatus.sql")
-load_run_sql(seeds_dir, "patient_status_master_patient_substatus_variables.sql")
-load_run_sql(seeds_dir, "patient_status_enrich_patient_journey_hierarchy_template.sql")
-load_run_sql(seeds_dir, "patient_status_enrich_patient_journey_hierarchy.sql")
-load_run_sql(seeds_dir, "patient_status_enrich_patient_journey_hierarchy_variables.sql")
-load_run_sql(seeds_dir, "patient_status_enrich_fill_null_long_pat_id.sql")
-load_run_sql(seeds_dir, "patient_status_ingest_brand_derivation.sql")
-load_run_sql(seeds_dir, "patient_status_enrich_fill_null_ref_date.sql")
-load_run_sql(seeds_dir, "enrich_referral_date_replace.sql")
-logging.info("Seed loading complete!")
-=======
 
 # List of all filenames in /seeds beginning with patient_status
-patient_status_seeds = glob(os.path.join(seeds_dir, 'patient_status*')) 
+patient_status_seeds = glob(os.path.join(seeds_dir, 'patient_status*'))
 patient_status_seeds = list(map(os.path.basename, patient_status_seeds))
 # patient_status_seeds.remove("filename.sql") to not load undesired patient_status seeds
 for seed in patient_status_seeds:
     load_run_sql(seeds_dir, seed)
 
 logging.info("Seed loading complete!")
->>>>>>> master
